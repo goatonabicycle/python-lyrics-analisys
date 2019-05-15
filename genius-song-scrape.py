c7d5eed23bf7adaf183bfe5b3bc0fccb8b306b2b
Rowan <malice.peanutbutter@gmail.com>
2019-05-15 22:40:05 +0200

    Removed old file

--- genius-song-scrape.py
+++
@@ -1,9 +0,0 @@
-import json
-import requests
-from bs4 import BeautifulSoup
-
-page = requests.get('https://genius.com/Aesop-rock-gopher-guts-lyrics')
-html = BeautifulSoup(page.text, "html.parser")
-
-lyrics = html.find("div", class_="lyrics").get_text()
-print(lyrics)
