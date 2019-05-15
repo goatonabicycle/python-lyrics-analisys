e4f397b573abb6f7601acffc34e76a72f1532f9d
Rowan <malice.peanutbutter@gmail.com>
2019-05-15 21:58:31 +0200

    Added exception handling to filehandling class

--- fileHandling.py
+++ fileHandling.py
@@ -1,8 +1,11 @@
 
 
-class fileHandling(object):
+class fileHandling(object):    
     def writeToFile(self, fileName, content):
-        f = open(fileName, "w+")
-        f.write(content)
-        f.close()
-        return "Success! I think."
+        try:
+            f = open(fileName, "w+")
+            f.write(content)
+            f.close()
+            return "Success! I think."
+        except:
+            return "Failed. Dismally."
